7e98db1d14f222594d8c9cf460e2c373ca776ee8
Idestru <idestru@yandex.ru>
2015-07-21 13:16:07 +0300

    Added front controller

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
 
 require_once 'config.php';
 require_once 'model.php';
+require_once 'controllers.php';
 
-$posts = get_all_posts();
+// route the request internally
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-require 'templates/list.php';
+if ('/index.php' == $uri) {
+    list_action();
+} elseif ('/index.php/show' == $uri && isset($_GET['id'])) {
+    show_action($_GET['id']);
+} else {
+    header('Status: 404 Not Found');
+    echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
